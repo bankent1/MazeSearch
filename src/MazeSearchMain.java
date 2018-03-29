6bf19bf0851035ac512d1c5974fb599f44c4c0d6
bankent1 <35464088+bankent1@users.noreply.github.com>
2018-03-28 23:07:54 -0700

    Add files via upload

---
+++ src/MazeSearchMain.java
@@ -0,0 +1,28 @@
+/**
+ * Author: Travis Banken
+ * 
+ * java MazeSolverMain infile.txt
+ * 
+ * Notes on Program:
+ * 
+ * 
+ */
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class MazeSearchMain {
+	
+	public static void main(String[] args) throws FileNotFoundException{
+		Scanner infile = new Scanner(new File(args[0]));
+		
+		// TODO: build maze
+		
+		// TODO: search maze
+	}
+	
+	public static void buildMaze(Scanner infile) {
+		
+	}
+}
